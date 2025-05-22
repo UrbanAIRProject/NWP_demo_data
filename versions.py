e4a84c04c721a1b246d30d56c7d5982bfb889cc6
Ulf.Andrae <a000864@c22681.ad.smhi.se>
2025-05-22 17:00:47 +0200

    Ulf Andrae: Update version access

--- versions.py
+++ versions.py
@@ -3,3 +3,14 @@ class UrbanAirData:
     urls = { 4: "http://exporter.nsc.liu.se/8a930c993fe54eedbdd7d4451b45ea57" }
     current_version = 4
     base_url = urls[current_version]
+
+def url_version(version):
+
+    try:
+       url = UrbanAirData.urls[version]
+    except KeyError:
+       print(f"Version {version} is not available")
+       print(f"Available versions are: {list(UrbanAirData.urls.keys())}")
+       url = None
+
+    return url
